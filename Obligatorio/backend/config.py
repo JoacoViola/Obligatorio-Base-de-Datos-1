5385a455d8708bf246e1b73ed4eec2bc18cbf9ac
Joaquin Viola <JOAQUIN.VIOLA@correo.ucu.edu.uy>
2025-11-21 16:27:57 -0300

    ¨Cambios¨

--- Obligatorio/backend/config.py
+++ Obligatorio/backend/config.py
@@ -1,4 +1,11 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "tu_contraseña"
-DB_NAME = "gestion_academica"
+import os
+from dotenv import load_dotenv
+
+# Cargar archivo .env si existe
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "gestion_academica")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
--- Obligatorio/backend/db.py
+++ Obligatorio/backend/db.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
-from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
+
 
 def get_connection():
     try:
@@ -8,9 +9,10 @@ def get_connection():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME
+            database=DB_NAME,
+            port=DB_PORT
         )
         return conn
     except Error as e:
-        print("Error conectando a MySQL:", e)
+        print(f"Error conectando a la base de datos: {e}")
         return None
